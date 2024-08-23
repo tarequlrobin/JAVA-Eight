934e921d27268f8fb7e0a046c91f6f21008cb3ac
Tareq Robin <tarequlrobin@gmail.com>
2024-08-23 12:36:27 +0600

    Initial Commit Stream API

--- src/StreamAPIImpl.java
+++ src/StreamAPIImpl.java
@@ -6,13 +6,13 @@ public class StreamAPIImpl {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("eat", "tea", "tan", "ate", "nat", "bat");
         Stream<String> stream = list.stream();
-        //stream.forEach(System.out::println);
+        stream.forEach(System.out::println);
 
         String [] strings = {"bat", "tan", "eat", "tea",  "ate", "nat"};
         Stream<String> stringsStream = Arrays.stream(strings);
-        //stringsStream.forEach(System.out::println);
+        stringsStream.forEach(System.out::println);
 
-        //Stream.of(list).forEach(System.out::print);
+        Stream.of(list).forEach(System.out::print);
 
         Stream <Integer> integerStream = Stream.iterate(1, n-> n+1).limit(list.size());
         integerStream.forEach(System.out::println);
